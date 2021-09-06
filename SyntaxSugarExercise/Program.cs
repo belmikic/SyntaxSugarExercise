b772d9e92486c520015dde990e348fd1c991a03f
belmikic <belmikic@gmail.com>
2021-09-05 22:31:57 -0700

    exercise completed

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"The number is less than nine" : "The number is greater than or equal to 9";
+
+            Console.WriteLine(response);
+
+        }
+    }
+}
